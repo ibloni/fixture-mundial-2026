baf2b835c06145caf60d66230d5038cedf94f4c2
Ale Nobili <ibloni@gmail.com>
2026-06-29 22:46:31 -0300

    mejora grupos

--- js/data/groups.js
+++ js/data/groups.js
@@ -1,14 +1,14 @@
 window.groups = {
-    A: ["MEX", "TBD-A2", "TBD-A3", "TBD-A4"],
-    B: ["CAN", "TBD-B2", "TBD-B3", "TBD-B4"],
-    C: ["USA", "TBD-C2", "TBD-C3", "TBD-C4"],
-    D: ["TBD-D1", "TBD-D2", "TBD-D3", "TBD-D4"],
-    E: ["TBD-E1", "TBD-E2", "TBD-E3", "TBD-E4"],
-    F: ["TBD-F1", "TBD-F2", "TBD-F3", "TBD-F4"],
-    G: ["TBD-G1", "TBD-G2", "TBD-G3", "TBD-G4"],
-    H: ["TBD-H1", "TBD-H2", "TBD-H3", "TBD-H4"],
-    I: ["TBD-I1", "TBD-I2", "TBD-I3", "TBD-I4"],
-    J: ["TBD-J1", "TBD-J2", "TBD-J3", "TBD-J4"],
-    K: ["TBD-K1", "TBD-K2", "TBD-K3", "TBD-K4"],
-    L: ["TBD-L1", "TBD-L2", "TBD-L3", "TBD-L4"]
+    A: ["MEX", "ZAF", "KOR", "CZE"],
+    B: ["CAN", "BIH", "QAT", "SUI"],
+    C: ["BRA", "MAR", "HTI", "SCO"],
+    D: ["USA", "PAR", "AUS", "TUR"],
+    E: ["GER", "CUW", "CIV", "ECU"],
+    F: ["NED", "JPN", "SWE", "TUN"],
+    G: ["BEL", "EGY", "IRN", "NZL"],
+    H: ["ESP", "CPV", "KSA", "URY"],
+    I: ["FRA", "SEN", "IRQ", "NOR"],
+    J: ["ARG", "ALG", "AUT", "JOR"],
+    K: ["POR", "COD", "UZB", "COL"],
+    L: ["ENG", "CRO", "GHA", "PAN"]
 };
